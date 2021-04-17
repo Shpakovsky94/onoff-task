1846c6d6872f1e54887622f27d9fd7395f300ea8
Oleks Oleks <shpakovsky94@gmail.com>
2021-04-17 18:25:50 +0300

    first push

---
+++ src/main/java/com/example/onofftask/OnoffTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.onofftask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnoffTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnoffTaskApplication.class, args);
+    }
+
+}
